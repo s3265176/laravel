b2cb9464308486c84e20f2b6d72e97c3d1f5e060
Jiheng Wang <wang1230ji@gmail.com>
2018-10-03 12:44:10 +1000

    update default view to Greenbeanbakehouse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,17 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Welcome To Green Bean Bake House</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <img src="{{ asset('img/green-bean-cake.jpg') }}">
+
+                    <br>
+
+                    <p>Green bean is a popular Chinses pastry. It is a vary delicious and healthy food</p>
+
+                    <p>Now you can order it Online. However currently can only product 50 eath day. so hurry up!</p> 
+
                 </div>
             </div>
         </div>
