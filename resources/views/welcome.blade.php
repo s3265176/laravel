b93be7811850ffeb65e495fe7684040be96b53f8
Jiheng Wang <wang1230ji@gmail.com>
2018-10-03 22:32:00 +1000

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,16 @@
                 <div class="panel-heading">Welcome To Green Bean Bake House</div>
 
                 <div class="panel-body">
-                    <img src="{{ asset('img/green-bean-cake.jpg') }}">
-
                     <br>
+                    <div class="jumbotron">
+                      <h1 class="display-4">Welcome</h1>
+                      <p class="lead">Green bean is a popular Chinses pastry. It is a vary delicious and healthy food</p>
 
-                    <p>Green bean is a popular Chinses pastry. It is a vary delicious and healthy food</p>
-
-                    <p>Now you can order it Online. However currently can only product 50 eath day. so hurry up!</p> 
-
+                      <img src="{{ asset('img/green-bean-cake.jpg') }}">
+                      
+                      <p>Now you can order it Online. However currently can only product 50 eath day. so hurry up!</p>
+                      <a class="btn btn-primary btn-lg" href="{{ url('/order/create') }}" role="button">Order Now</a>
+                    </div>
                 </div>
             </div>
         </div>
